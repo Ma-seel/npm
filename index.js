d4b760bb71775fb8384c54614690e1d8a8008d5f
Ma-seel <179339723+Ma-seel@users.noreply.github.com>
2024-11-24 15:41:28 +0500

    Updated package

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+
 function add(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error('Both arguments must be numbers');
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { add, subtract, multiply, divide } = require('./index');
 
-console.log(add(2, 3));         // Output: 5
-console.log(subtract(5, 3));    // Output: 2
-console.log(multiply(2, 3));    // Output: 6
-console.log(divide(6, 3));      // Output: 2
+console.log(add(2, 3));         
+console.log(subtract(5, 3));    
+console.log(multiply(2, 3));    
+console.log(divide(6, 3));     
